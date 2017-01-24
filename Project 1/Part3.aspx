3f0a4ee62339b3ba240d9e1f0decde58c57345bb
trcoats <trcoats@bsu.edu>
2017-01-24 12:54:33 -0500

    Complete Part3

--- Project 1/Part3.aspx
+++ Project 1/Part3.aspx
@@ -8,8 +8,30 @@
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+    <div style="text-align: left">
     <h3><a href="Part1.aspx">Part 1</a> <a href="Part2.aspx">Part 2</a> Part 3 <a href="Part4.aspx">Part 4</a></h3>
+        <br />
+        Quizzes:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="tbx_QuizScore" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Assignments:&nbsp;&nbsp;
+        <asp:TextBox ID="tbx_AssignmentScore" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Midterm:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="tbx_Midterm" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Final:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="tbx_FinalScore" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btn_Calculate" runat="server" OnClick="btn_Calculate_Click" Text="Calculate" />
+        <br />
+        <br />
+        <asp:Label ID="lbl_Output" runat="server"></asp:Label>
+        <br />
     </div>
     </form>
 </body>
--- Project 1/Part3.aspx.cs
+++ Project 1/Part3.aspx.cs
@@ -12,5 +12,58 @@ namespace Project_1
         protected void Page_Load(object sender, EventArgs e) {
 
         }
+
+        protected void btn_Calculate_Click(object sender, EventArgs e)
+        {
+            double gradeScore = CalculateGrade();
+            string letterGrade = AssignLetterGrade(gradeScore);
+
+            lbl_Output.Text = "Received a final grade of " + letterGrade + " with a score of " + gradeScore + "%";
+        }
+
+        private double CalculateGrade()
+        {
+            double avgGrade = 0.00;
+            double quizScore = double.Parse(tbx_QuizScore.Text);
+            double assignmentScore = double.Parse(tbx_AssignmentScore.Text);
+            double midtermScore = double.Parse(tbx_Midterm.Text);
+            double finalScore = double.Parse(tbx_FinalScore.Text);
+
+            avgGrade = (quizScore * .15) + (assignmentScore * .40) + (midtermScore * .20) + (finalScore * .25);
+
+            return avgGrade;
+        }
+
+        private string AssignLetterGrade(double grade)
+        {
+            string finalGrade = "";
+
+            if (grade <= 100 && grade > 90)
+            {
+                finalGrade = "A";
+            }
+            else if (grade <= 90 && grade > 80)
+            {
+                finalGrade = "B";
+            }
+            else if (grade <= 80 && grade > 70)
+            {
+                finalGrade = "C";
+            }
+            else if (grade <= 70 && grade > 60)
+            {
+                finalGrade = "D";
+            }
+            else if (grade <= 60)
+            {
+                finalGrade = "F";
+            }
+            else
+            {
+                finalGrade = "Something went wrong!";
+            }
+
+            return finalGrade;
+        }
     }
 }
